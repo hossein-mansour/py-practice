1733ebaf6e1117c608965dd6e5005188c59a4a26
Hossein Mansour <hossein_mansour63@yahoo.com>
2020-07-16 14:07:09 -0700

    revised to include non-recursive (reference) version as well

--- recursive_file_search.py
+++ recursive_file_search.py
@@ -1,20 +1,33 @@
 '''
-Find all files in subdirectories recursively
+Find all files in subdirectories recursively. A bit dumb but just a practice of writing recursive stuff
 July 16th 2020
 '''
 import os
 
 my_path = "C:\\Users\\hosse\\Desktop"
-subfolder = True
+
 def all_filenames(my_path):
-	for folderName, subfolders, filenames in os.walk(my_path):
+	for folderName, subfolders, filenames in list(os.walk(my_path)):
+		print('Files in ' + folderName + ':')
+		for file in filenames:
+			print(os.path.join(folderName,file))
+		print("")
+
+
+def all_filenames_recursive(my_path):
+
+	for folderName, subfolders, filenames in list(os.walk(my_path))[:1]:
 		print('Files in ' + folderName + ':')
 		for file in filenames:
 			print(os.path.join(folderName,file))
 		print("")
 		if subfolders:
 			for subfolder in subfolders:
-				all_filenames(os.path.join(folderName,subfolder))
+				all_filenames_recursive(os.path.join(folderName,subfolder))
+				
 				
 if __name__ == "__main__":
-	all_filenames(my_path)
+	print("non-Recursive version (Reference)")
+	all_filenames(my_path)	
+	print("Recursive version")
+	all_filenames_recursive(my_path)
